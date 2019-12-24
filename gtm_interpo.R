0c56628aa3e7517a36d4a1288e4ec640bf43a00b
jetstreamokayasu <tokotsuku2@outlook.jp>
2019-12-25 04:03:05 +0900

    GTMを用いた補間。中心点のボロノイ領域の頂点への補間は成功

---
+++ gtm_interpo.R
@@ -0,0 +1,104 @@
+require(TDA)
+require(myfs)
+require(rgl)
+require(phacm)
+require(pracma)
+require(deldir)
+require(ggplot2)
+require(plyr)
+require(reshape2)
+require(ggmap)
+require(tidyverse)
+require(interpo3d)
+require(seephacm)
+require(gtm)
+
+#GTMを用いた補間
+
+torus300<-torusUnif(300, 1, 2.5)
+
+torus300_dist<-dist(torus300)
+trs300_vics1<-interpo3d:::get_vicinity(torus300_dist, 1, 30)
+figurePlot3d(torus300[-trs300_vics1, ])
+points3d(torus300[trs300_vics1, ], col=2)
+spheres3d(rbind(c(0, 0, 0), torus300[trs300_vics1[1], ]), radius = 0.1, col=3)
+
+X<-torus300[trs300_vics1, ]
+# (分散が0の変数を削除した後に) 1. オートスケーリング
+Var0Variable <- which(apply(X,2,var) == 0)
+if (length(Var0Variable) == 0) {
+  print("分散が0の変数はありません")
+} else {
+  sprintf("分散が0の変数が %d つありました", length(Var0Variable))
+  print( "変数:" )
+  print( Var0Variable )
+  print( "これらを削除します" )
+  X <- X[,-Var0Variable]
+}
+X <- scale(X, center = TRUE, scale = TRUE)
+# 2. マップサイズ
+MapsizeColumn = 15 #横 10
+MapsizeRow = 15 #縦 10 、
+# 3. 動径基底関数 (Radial Basis Function, RBF) の数
+RBFsizeColumn = 3 #横 3
+RBFsizeRow = 3 #縦 3
+# 4. ガウス関数の分散
+RBFVariance = 1
+# 5. EMアルゴリズムにおけるパラメータλの値
+Lambda = 0
+# 6. データ空間における分散の逆数βの初期値
+# 7. 重みWの初期値
+XGrid = gtm.rctg( MapsizeColumn, MapsizeRow)#写像先のグリッド
+RBFGrid = gtm.rctg( RBFsizeColumn, RBFsizeRow)#基底関数のグリッド
+RBFSetup = gtm.gbf( RBFGrid, RBFVariance^(1/2), XGrid)
+InitnalWBeta = gtm.pci.beta( X, XGrid, RBFSetup)
+#Beta = 0.01
+Beta = InitnalWBeta$beta
+# 8. 学習回数
+NumOfTraining = 100 #Training cycleとlog-likelihoodとのプロットが収束していないようでしたら学習回数を大きくしてください
+# 9. GTMマップ作成
+GTMResults = gtm.trn( X, RBFSetup, InitnalWBeta$W, Lambda, NumOfTraining, Beta)
+# 10. 二次元のマップ上でサンプルの位置関係を確認
+GTMDist = gtm.dist(X, RBFSetup %*% GTMResults$W)
+minDist = apply(GTMDist, 2, which.min)
+GTMMode = matrix(XGrid[minDist, ], ncol = 2)
+GTMR = gtm.resp3(GTMDist, GTMResults$beta, ncol(X))$R
+GTMMean = t(GTMR) %*% XGrid
+par(pty = "s")
+plot( GTMMode[,1], GTMMode[,2], xlim=c(-1.2, 1.2), ylim=c(-1.2, 1.2), col = 'blue', xlab = "z1", ylab = "z2")
+text( GTMMode[,1], GTMMode[,2], labels = rownames(X), pos=3, offset = 0.1)
+par(pty = "m")
+par(pty = "s")
+plot( GTMMean[,1], GTMMean[,2], xlim=c(-1.2, 1.2), ylim=c(-1.2, 1.2), col = 'green', xlab = "z1", ylab = "z2", pch=16)
+text( GTMMean[,1], GTMMean[,2], labels = 1:72, pos=3, offset = 0.1)
+par(pty = "m")
+
+#次元削減後のデータ点にラベルを付ける
+library(maptools)
+pointLabel(GTMMode[,1], GTMMode[,2], as.character(trs300_vics1))
+
+#逆写像を求める
+ori_GTMMean = t(GTMR) %*% (RBFSetup %*% GTMResults$W)
+##スケーリングを戻す
+inv_GTMMean<-sapply(1:nrow(ori_GTMMean), function(k){ori_GTMMean[k, ] * attr(X, "scaled:scale") + attr(X, "scaled:center")})
+inv_GTMMean<-t(inv_GTMMean)
+
+points3d(inv_GTMMean, col=3)
+
+
+#次元削減後にボロノイ分割
+points(GTMMean[1,1], GTMMean[1,2], pch=16, col=4)
+trs300_res1<-deldir(GTMMean[,1], GTMMean[,2])
+trs300_tiles1<-tile.list(trs300_res1)
+for(i in 1:trs300_res1$n.data){	polygon(trs300_tiles1[[i]], lwd=2) }
+
+trs300_vertx1<-cbind(trs300_tiles1[[1]][["x"]], trs300_tiles1[[1]][["y"]])
+points(trs300_tiles1[[1]][["x"]], trs300_tiles1[[1]][["y"]], pch=16, col=2, cex=1)
+
+RBF_inter = gtm.gbf( RBFGrid, RBFVariance^(1/2), cbind(trs300_tiles1[[1]][["x"]], trs300_tiles1[[1]][["y"]]))
+inter_dist<-gtm.dist(X, RBF_inter %*% GTMResults$W)
+inter_R = gtm.resp3(inter_dist, GTMResults$beta, ncol(X))$R
+inter_mean = t(inter_R) %*% (RBF_inter %*% GTMResults$W)
+inter_inv<-sapply(1:nrow(inter_mean), function(k){inter_mean[k, ] * attr(X, "scaled:scale") + attr(X, "scaled:center")})
+inter_inv<-t(inter_inv)
+points3d(inter_inv, col=4)
